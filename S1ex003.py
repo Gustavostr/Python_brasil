f9c13c305a1f23b62528ccdda35bc5d2c8f5ecac
gustavostr <gao.gustavo@gmail.com>
2023-09-09 21:59:54 -0300

    Lista do python Brasil

---
+++ S1ex003.py
@@ -0,0 +1,4 @@
+#Faça um Programa que peça dois números e imprima a soma.
+numero1 = float(input("Digite um número qualquer: "))
+numero2 = float (input("Digite outro número qualquer: "))
+print ("A soma dos dois valores informados são {}".format(numero1 + numero2))
