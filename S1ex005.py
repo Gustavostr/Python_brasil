58eb49068c9ad979408d62f4b3140224ddae83b1
gustavostr <gao.gustavo@gmail.com>
2023-09-09 22:29:57 -0300

    Python Brasil

---
+++ S1ex005.py
@@ -0,0 +1,4 @@
+#Faça um Programa que converta metros para centímetros.
+metro = float(input("Digite a metragem: "))
+cen = metro*100
+print("a metragem informada são {} em centímetros".format(cen))
